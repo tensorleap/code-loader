c30e89909132ea7e4b244a0571df73b37bb812cd
dorhar <doron.harnoy@tensorleap.ai>
2021-11-22 15:15:16 +0200

    add fixtures utils

---
+++ tests/fixtures/utils.py
@@ -0,0 +1,16 @@
+import pytest
+
+from code_loader import dataset_binder
+from code_loader.contract.datasetclasses import DatasetIntegrationSetup
+
+
+def use_fixture(fixture_func):
+    def inner(test_func):
+        return pytest.mark.usefixtures(fixture_func.__name__)(test_func)
+
+    return inner
+
+
+@pytest.fixture
+def refresh_setup_container():
+    dataset_binder.setup_container = DatasetIntegrationSetup()
