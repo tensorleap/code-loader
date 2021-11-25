c74b191bc2b92b8e613b789dc8fdfab2802fee08
dorhar <doron.harnoy@tensorleap.ai>
2021-11-25 16:37:41 +0200

    add test assertions

--- tests/assertions.py
+++ tests/assertions.py
@@ -1,5 +1,11 @@
+from typing import Dict, List
+
+import numpy as np
+
 from code_loader import dataset_binder
 from grappa import should  # type: ignore
+
+from code_loader.contract.datasetclasses import SubsetResponse, DatasetSample
 
 
 def assert_dataset_binder_is_valid():
@@ -9,3 +15,23 @@ def assert_dataset_binder_is_valid():
     len(setup_container.inputs) | should.be.higher.than(0)
     len(setup_container.ground_truths) | should.be.higher.than(0)
     len(setup_container.metadata) | should.be.higher.than(0)
+
+
+def assert_subsets_is_valid(subsets: Dict[str, List[SubsetResponse]]):
+    for subset_name, list_subset_response in subsets.items():
+        subset_name | should.be.type(str)
+        for subset_response in list_subset_response:
+            subset_response | should.be.type(SubsetResponse)
+
+
+def assert_encoder_is_valid(encoder_result: Dict[str, np.ndarray]):
+    for encoder_name, encoder_data in encoder_result.items():
+        encoder_name | should.be.type(str)
+        type(encoder_data) | should.be.type(type(np.ndarray))
+
+
+def assert_sample_is_valid(sample: DatasetSample):
+    sample | should.be.type(DatasetSample)
+    assert_encoder_is_valid(sample.inputs)
+    assert_encoder_is_valid(sample.gt)
+    assert_encoder_is_valid(sample.metadata)
