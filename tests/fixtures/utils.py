381cb6f41b1d8eac55e2d61143664a1b98f817cb
dorhar <doron.harnoy@tensorleap.ai>
2021-11-25 16:38:14 +0200

    add params fixture to sample function

--- tests/fixtures/utils.py
+++ tests/fixtures/utils.py
@@ -1,7 +1,10 @@
+from typing import Union, Dict
+
 import pytest
 
 from code_loader import dataset_binder
 from code_loader.contract.datasetclasses import DatasetIntegrationSetup
+from code_loader.contract.enums import DataStateEnum
 
 
 def use_fixture(fixture_func):
@@ -14,3 +17,8 @@ def use_fixture(fixture_func):
 @pytest.fixture
 def refresh_setup_container() -> None:
     dataset_binder.setup_container = DatasetIntegrationSetup()
+
+
+@pytest.fixture
+def simple_sample_params() -> Dict[str, Union[DataStateEnum, int]]:
+    return {"state": DataStateEnum.training, "idx": 0}
