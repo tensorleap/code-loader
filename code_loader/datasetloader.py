06db0f9c90546844b9666e44d35fbd220aa9362c
dorhar <doron.harnoy@tensorleap.ai>
2021-11-25 16:42:15 +0200

    add get sample logic

--- code_loader/datasetloader.py
+++ code_loader/datasetloader.py
@@ -1,4 +1,11 @@
-from typing import Dict
+from functools import lru_cache
+from typing import Dict, List
+
+import numpy as np
+
+from code_loader import dataset_binder
+from code_loader.contract.datasetclasses import SubsetResponse, DatasetSample
+from code_loader.contract.enums import DataStateEnum
 
 
 class DatasetLoader:
@@ -10,3 +17,52 @@ class DatasetLoader:
 
     def exec_script(self):
         exec(self.dataset_script, self.global_variables)
+
+    def get_sample(self, state: DataStateEnum, idx: int) -> DatasetSample:
+        sample = DatasetSample(inputs=self._get_inputs(state, idx),
+                               gt=self._get_gt(state, idx),
+                               metadata=self._get_metadata(state, idx),
+                               index=idx,
+                               state=state)
+        return sample
+
+    @lru_cache()
+    def _subsets(self) -> Dict[str, List[SubsetResponse]]:
+        subsets: Dict[str, List[SubsetResponse]] = {}
+        for subset in dataset_binder.setup_container.subsets:
+            subset_result = subset.function()
+            subsets[subset.name] = subset_result
+        return subsets
+
+    def _get_inputs(self, state: DataStateEnum, idx: int) -> Dict[str, np.ndarray]:
+        result_agg = {}
+        subsets = self._subsets()
+        for input_handler in dataset_binder.setup_container.inputs:
+            subset = subsets[input_handler.subset_name]
+            subset_state = subset[state]
+            input_result = input_handler.function(idx, subset_state)
+            input_name = input_handler.name
+            result_agg[input_name] = input_result
+        return result_agg
+
+    def _get_gt(self, state: DataStateEnum, idx: int) -> Dict[str, np.ndarray]:
+        result_agg = {}
+        subsets = self._subsets()
+        for gt_handler in dataset_binder.setup_container.ground_truths:
+            subset = subsets[gt_handler.subset_name]
+            subset_state = subset[state]
+            gt_result = gt_handler.function(idx, subset_state)
+            gt_name = gt_handler.name
+            result_agg[gt_name] = gt_result
+        return result_agg
+
+    def _get_metadata(self, state: DataStateEnum, idx: int) -> Dict[str, np.ndarray]:
+        result_agg = {}
+        subsets = self._subsets()
+        for metadata_handler in dataset_binder.setup_container.metadata:
+            subset = subsets[metadata_handler.subset_name]
+            subset_state = subset[state]
+            metadata_result = metadata_handler.function(idx, subset_state)
+            metadata_name = metadata_handler.name
+            result_agg[metadata_name] = metadata_result
+        return result_agg
