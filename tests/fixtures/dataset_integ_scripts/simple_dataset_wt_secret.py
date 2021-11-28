68dbf6e56b9ec1fe493aa686215a817c65d40b15
dorhar <doron.harnoy@tensorleap.ai>
2021-11-28 17:17:41 +0200

    create dataset integ script that uses secret

---
+++ tests/fixtures/dataset_integ_scripts/simple_dataset_wt_secret.py
@@ -0,0 +1,106 @@
+import os
+from typing import List
+
+import numpy as np  # type: ignore
+
+from code_loader import dataset_binder
+from code_loader.contract.datasetclasses import SubsetResponse
+from code_loader.contract.enums import DatasetInputType, DatasetOutputType, DatasetMetadataType
+
+SECRET = os.getenv("SECRET")
+print(SECRET)
+
+
+def get_length(data):
+    if data is None:
+        length = None
+    elif type(data) is dict and 'length' in data:
+        length = data['length']
+    elif type(data) is not dict:
+        length = len(data)
+    else:
+        length = None
+
+    return length
+
+
+def subset_test_subset_1_10() -> List[SubsetResponse]:
+    a = [0] * 4
+    b = [0] * 2
+    c = [0]
+    return [SubsetResponse(length=get_length(a), data=np.array(a)),
+            SubsetResponse(length=get_length(b), data=np.array(b)),
+            SubsetResponse(length=get_length(c), data=np.array(c))]
+
+
+def subset_test_subset_11_20() -> List[SubsetResponse]:
+    a = [1] * 8
+    b = [1] * 2
+    c = [1]
+    return [SubsetResponse(length=get_length(a), data=np.array(a)),
+            SubsetResponse(length=get_length(b), data=np.array(b)),
+            SubsetResponse(length=get_length(c), data=np.array(c))]
+
+
+def input_normal_input_subset_1_10(idx, samples):
+    samples = samples.data
+    batch_x = samples[idx: idx + 1]
+    batch_images = []
+    for x in batch_x:
+        batch_images.append(x)
+    return batch_images[0]
+
+
+def input_negative_input_subset_11_20(idx, samples):
+    samples = samples.data
+    batch_x = samples[idx: idx + 1]
+    batch_images = []
+    for x in batch_x:
+        batch_images.append(x * (-1))
+    return batch_images[0]
+
+
+def ground_truth_output_times_20(idx, samples):
+    samples = samples.data
+    batch_x = samples[idx: idx + 1]
+    batch_images = []
+    for x in batch_x:
+        batch_images.append(x * 20)
+    return batch_images[0]
+
+
+def metadata_x(idx, samples):
+    samples = samples.data
+    batch_x = samples[idx: idx + 1]
+    batch_metafata = []
+    for x in batch_x:
+        batch_metafata.append(0)
+    return batch_metafata[0]
+
+
+def metadata_y(idx, samples):
+    samples = samples.data
+    batch_x = samples[idx: idx + 1]
+    batch_metafata = []
+    for x in batch_x:
+        batch_metafata.append("fake_string")
+    return batch_metafata[0]
+
+
+dataset_binder.set_subset(0.8, subset_test_subset_1_10, 'test_subset_1_10')
+
+dataset_binder.set_subset(0.2, subset_test_subset_11_20, 'test_subset_11_20')
+
+dataset_binder.set_input(input_normal_input_subset_1_10, 'test_subset_1_10', DatasetInputType.Numeric,
+                         'normal_input_subset_1_10')
+
+dataset_binder.set_input(input_negative_input_subset_11_20, 'test_subset_11_20', DatasetInputType.Numeric,
+                         'negative_input_subset_11_20')
+
+dataset_binder.set_ground_truth(ground_truth_output_times_20, 'test_subset_11_20', DatasetOutputType.Numeric,
+                                'output_times_20',
+                                labels=None, masked_input=None)
+
+dataset_binder.set_metadata(metadata_x, 'test_subset_1_10', DatasetMetadataType.int, 'x')
+
+dataset_binder.set_metadata(metadata_y, 'test_subset_1_10', DatasetMetadataType.string, 'y')
