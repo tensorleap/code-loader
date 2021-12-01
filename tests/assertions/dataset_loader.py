e4742a50c3007698032837ab694d7f686348a744
dorhar <doron.harnoy@tensorleap.ai>
2021-12-01 18:44:58 +0200

    add types to assertions

--- tests/assertions/dataset_loader.py
+++ tests/assertions/dataset_loader.py
@@ -9,7 +9,7 @@ from code_loader.contract.datasetclasses import SubsetResponse, DatasetSample
 from code_loader.datasetloader import DatasetLoader
 
 
-def assert_dataset_binder_is_valid():
+def assert_dataset_binder_is_valid() -> None:
     setup_container = dataset_binder.setup_container
 
     len(setup_container.subsets) | should.be.higher.than(0)
@@ -18,14 +18,14 @@ def assert_dataset_binder_is_valid():
     len(setup_container.metadata) | should.be.higher.than(0)
 
 
-def assert_subsets_is_valid(subsets: Dict[str, List[SubsetResponse]]):
+def assert_subsets_is_valid(subsets: Dict[str, List[SubsetResponse]]) -> None:
     for subset_name, list_subset_response in subsets.items():
         subset_name | should.be.type(str)
         for subset_response in list_subset_response:
             subset_response | should.be.type(SubsetResponse)
 
 
-def assert_encoder_is_valid(encoder_result: Dict[str, np.ndarray]):
+def assert_encoder_is_valid(encoder_result: Dict[str, np.ndarray]) -> None:
     for encoder_name, encoder_data in encoder_result.items():
         encoder_name | should.be.type(str)
         type(encoder_data) | should.be.type(type(np.ndarray))
@@ -38,5 +38,5 @@ def assert_sample_is_valid(sample: DatasetSample):
     assert_encoder_is_valid(sample.metadata)
 
 
-def assert_secret_exists(dataset_loader: DatasetLoader):
+def assert_secret_exists(dataset_loader: DatasetLoader) -> None:
     dataset_loader.global_variables | should.have.key('SECRET').that.should.be.type(str)
