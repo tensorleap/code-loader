cb7d080785d388a3585b503b2dc85d79612378c3
dorhar <doron.harnoy@tensorleap.ai>
2021-12-07 15:52:09 +0200

    add test of word index

--- tests/fixtures/dataset_loaders.py
+++ tests/fixtures/dataset_loaders.py
@@ -22,3 +22,12 @@ def secret_dataset_loader() -> DatasetLoader:
         script = f.read()
     dataset_loader = DatasetLoader(script)
     return dataset_loader
+
+
+@pytest.fixture
+def word_idx_dataset_loader() -> DatasetLoader:
+    script_path = os.path.join(dataset_integ_scripts_path, "simple_no_cloud_dataset_wt_word_idx.py")
+    with open(script_path, "r") as f:
+        script = f.read()
+    dataset_loader = DatasetLoader(script)
+    return dataset_loader
--- tests/test_dataset_loader.py
+++ tests/test_dataset_loader.py
@@ -1,7 +1,7 @@
-from tests.assertions.dataset_loader import assert_dataset_binder_is_valid, assert_subsets_is_valid, assert_encoder_is_valid, \
-    assert_sample_is_valid, assert_secret_exists
-from tests.fixtures.dataset_loaders import no_cloud_dataset_loader, secret_dataset_loader
-from tests.fixtures.secrets.tensorleap_demo_secret import put_mock_secret_in_env
+from tests.assertions.dataset_loader import assert_dataset_binder_is_valid, assert_subsets_is_valid, \
+    assert_encoder_is_valid, assert_sample_is_valid, assert_word_to_index_in_cache_container, assert_input_has_value
+from tests.fixtures.dataset_integ_scripts.scripts_metadata import word_idx_dataset_params
+from tests.fixtures.dataset_loaders import no_cloud_dataset_loader, word_idx_dataset_loader
 from tests.fixtures.utils import refresh_setup_container
 from tests.fixtures.utils import use_fixture, simple_sample_params
 
@@ -72,7 +72,6 @@ def test_get_metadata_no_cloud(no_cloud_dataset_loader, refresh_setup_container,
 @use_fixture(simple_sample_params)
 def test_get_sample_dataset_loader_no_cloud(no_cloud_dataset_loader, refresh_setup_container, simple_sample_params):
     # act
-    no_cloud_dataset_loader.exec_script()
     sample = no_cloud_dataset_loader.get_sample(**simple_sample_params)
 
     # assert
@@ -80,8 +79,33 @@ def test_get_sample_dataset_loader_no_cloud(no_cloud_dataset_loader, refresh_set
     assert_sample_is_valid(sample)
 
 
+@use_fixture(word_idx_dataset_loader)
 @use_fixture(refresh_setup_container)
+@use_fixture(word_idx_dataset_params)
+def test_word_to_index(word_idx_dataset_loader, refresh_setup_container, word_idx_dataset_params):
     # act
+    word_idx_dataset_loader.exec_script()
+    word_idx_dataset_loader._subsets()
 
     # assert
     assert_dataset_binder_is_valid()
+    assert_word_to_index_in_cache_container(word_idx_dataset_params["input_name"],
+                                            word_idx_dataset_params["word_to_index_value"])
+
+
+@use_fixture(word_idx_dataset_loader)
+@use_fixture(refresh_setup_container)
+@use_fixture(simple_sample_params)
+@use_fixture(word_idx_dataset_params)
+def test_cache_container_in_encoder(word_idx_dataset_loader, refresh_setup_container, simple_sample_params,
+                                    word_idx_dataset_params):
+    # act
+    word_idx_dataset_loader.exec_script()
+    inputs = word_idx_dataset_loader._get_inputs(**simple_sample_params)
+
+    # assert
+    assert_dataset_binder_is_valid()
+    assert_word_to_index_in_cache_container(word_idx_dataset_params["input_name"],
+                                            word_idx_dataset_params["word_to_index_value"])
+    assert_input_has_value(inputs[word_idx_dataset_params["input_name"]],
+                           word_idx_dataset_params["word_to_index_value"])
