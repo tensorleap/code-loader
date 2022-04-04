a0ed0ea349c98739bf1251715c4ed30c27e5e63f
Idan <46688952+tip3x@users.noreply.github.com>
2022-04-04 17:19:52 +0300

    rename heatmap_block to prediction type and add metrics

--- code_loader/datasetloader.py
+++ code_loader/datasetloader.py
@@ -161,7 +161,7 @@ class DatasetLoader:
                     for decoder_handler in setup.decoders]
 
         return DatasetSetup(preprocess=dataset_preprocess, inputs=inputs, outputs=ground_truths, metadata=metadata,
-                            decoders=decoders, heatmap_blocks=setup.heatmap_blocks)
+                            decoders=decoders, prediction_types=setup.prediction_types)
 
     @lru_cache()
     def _preprocess_result(self) -> List[PreprocessResponse]:
