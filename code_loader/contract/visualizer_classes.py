7686ff3e869fa51aff9197b9b1544184a7d31eb4
Idan <46688952+tip3x@users.noreply.github.com>
2022-06-27 13:52:54 +0300

    add validations to leap objects

--- code_loader/contract/visualizer_classes.py
+++ code_loader/contract/visualizer_classes.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any
 
 import numpy as np
 import numpy.typing as npt
@@ -7,10 +7,35 @@ from dataclasses import dataclass
 from code_loader.contract.enums import LeapDataType
 
 
+class LeapValidationError(Exception):
+    pass
+
+
+def validate_type(actual: Any, expected: Any, prefix_message=''):
+    if not isinstance(expected, list):
+        expected = [expected]
+    if actual not in expected:
+        if len(expected) == 1:
+            raise LeapValidationError(
+                f'{prefix_message}.\n'
+                f'visualizer returned unexpected type. got {actual}, instead of {expected[0]}')
+        else:
+            raise LeapValidationError(
+                f'{prefix_message}.\n'
+                f'visualizer returned unexpected type. got {actual}, allowed is one of {expected}')
+
+
 @dataclass
 class LeapImage:
     data: npt.NDArray[np.float32]
     type: LeapDataType = LeapDataType.Image
+
+    def __post_init__(self):
+        validate_type(self.type, LeapDataType.Image)
+        validate_type(type(self.data), np.ndarray)
+        validate_type(self.data.dtype, [np.uint8, np.float32])
+        validate_type(len(self.data.shape), 3, 'Image must be of shape 3')
+        validate_type(self.data.shape[2], [1, 3], 'Image channel must be either 3(rgb) or 1(gray)')
 
 
 @dataclass
@@ -25,11 +50,22 @@ class LeapGraph:
     data: npt.NDArray[np.float32]
     type: LeapDataType = LeapDataType.Graph
 
+    def __post_init__(self):
+        validate_type(self.type, LeapDataType.Graph)
+        validate_type(type(self.data), np.ndarray)
+        validate_type(self.data.dtype, np.float32)
+        validate_type(len(self.data.shape), 2, 'Graph must be of shape 2')
+
 
 @dataclass
 class LeapText:
     data: List[str]
     type: LeapDataType = LeapDataType.Text
+
+    def __post_init__(self):
+        validate_type(self.type, LeapDataType.Text)
+        validate_type(type(self.data), list)
+        validate_type(type(self.data[0]), str)
 
 
 @dataclass
@@ -37,6 +73,15 @@ class LeapHorizontalBar:
     body: npt.NDArray[np.float32]
     labels: List[str]
     type: LeapDataType = LeapDataType.HorizontalBar
+
+    def __post_init__(self):
+        validate_type(self.type, LeapDataType.HorizontalBar)
+        validate_type(type(self.body), np.ndarray)
+        validate_type(self.body.dtype, np.float32)
+        validate_type(len(self.body.shape), 1, 'HorizontalBar body must be of shape 1')
+
+        validate_type(type(self.labels), list)
+        validate_type(type(self.labels[0]), str)
 
 
 @dataclass
@@ -46,6 +91,18 @@ class LeapImageMask:
     labels: List[str]
     type: LeapDataType = LeapDataType.ImageMask
 
+    def __post_init__(self):
+        validate_type(self.type, LeapDataType.ImageMask)
+        validate_type(type(self.mask), np.ndarray)
+        validate_type(self.mask.dtype, np.float32)
+        validate_type(len(self.mask.shape), 2, 'image mask must be of shape 2')
+        validate_type(type(self.image), np.ndarray)
+        validate_type(self.image.dtype, np.float32)
+        validate_type(len(self.image.shape), 3, 'Image must be of shape 3')
+        validate_type(self.image.shape[2], [1, 3], 'Image channel must be either 3(rgb) or 1(gray)')
+        validate_type(type(self.labels), list)
+        validate_type(type(self.labels[0]), str)
+
 
 @dataclass
 class LeapTextMask:
@@ -53,3 +110,13 @@ class LeapTextMask:
     text_array: npt.NDArray[np.float32]
     labels: List[str]
     type: LeapDataType = LeapDataType.TextMask
+
+    def __post_init__(self):
+        validate_type(self.type, LeapDataType.ImageMask)
+        validate_type(type(self.mask), np.ndarray)
+        validate_type(self.mask.dtype, np.float32)
+        validate_type(len(self.mask.shape), 1, 'text mask must be of shape 1')
+        validate_type(type(self.text_array), np.ndarray)
+        validate_type(self.text_array.dtype, np.float32)
+        validate_type(type(self.labels), list)
+        validate_type(type(self.labels[0]), str)
