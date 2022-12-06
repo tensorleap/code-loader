4b639fd270788dc36d4f6a463a6720d4d4fd3ef5
dorhar <doron.harnoy@tensorleap.ai>
2022-12-06 16:46:21 +0200

    fix tests for metric reponse

--- tests/fixtures/dataset_integ_scripts/simple_no_cloud_dataset_wt_visualizer.py
+++ tests/fixtures/dataset_integ_scripts/simple_no_cloud_dataset_wt_visualizer.py
@@ -2,10 +2,11 @@ from typing import List
 
 import numpy as np
 import numpy.typing as npt
+import tensorflow as tf
 
 from code_loader import leap_binder
-from code_loader.contract.datasetclasses import PreprocessResponse
-from code_loader.contract.enums import DatasetMetadataType, LeapDataType
+from code_loader.contract.datasetclasses import PreprocessResponse, ConfusionMatrixElement
+from code_loader.contract.enums import DatasetMetadataType, LeapDataType, ConfusionMatrixValue
 from code_loader.contract.visualizer_classes import LeapText
 
 
@@ -71,7 +72,8 @@ def raw_custom_visualizer_func(data: npt.NDArray) -> LeapText:
     return LeapText([str(data)])
 
 
-leap_binder.set_visualizer(function=raw_custom_visualizer_func, name='stub_visualizer', visualizer_type=LeapDataType.Text)
+leap_binder.set_visualizer(function=raw_custom_visualizer_func, name='stub_visualizer',
+                           visualizer_type=LeapDataType.Text)
 
 leap_binder.set_preprocess(function=prepare_data)
 
@@ -88,4 +90,32 @@ def custom_metric(pred, gt):
     return pred - gt
 
 
+leap_binder.add_custom_metric(custom_metric, "custom_metric")
+
 leap_binder.add_prediction(name='pred_type1', labels=['yes', 'no'])
+
+
+def custom_confusion_metric(gt_one_hot_encoding: tf.Tensor, pred_probabilities: tf.Tensor) -> List[
+    List[ConfusionMatrixElement]]:
+    num_labels = pred_probabilities.shape[-1]
+    labels = list(range(num_labels))
+    if len(labels) == 1:
+        labels = ['0', '1']
+        gt_one_hot_encoding = tf.concat([1 - gt_one_hot_encoding, gt_one_hot_encoding], axis=1)
+        pred_probabilities = tf.concat([1 - pred_probabilities, pred_probabilities], axis=1)
+
+    ret = []
+    for batch_i in range(gt_one_hot_encoding.shape[0]):
+        one_hot_vec = list(gt_one_hot_encoding[batch_i])
+        pred_vec = list(pred_probabilities[batch_i])
+        confusion_matrix_elements = []
+        for i, label in enumerate(labels):
+            expected_outcome = ConfusionMatrixValue.Positive if int(
+                one_hot_vec[i]) == 1 else ConfusionMatrixValue.Negative
+            cm_element = ConfusionMatrixElement(label, expected_outcome, float(pred_vec[i]))
+            confusion_matrix_elements.append(cm_element)
+        ret.append(confusion_matrix_elements)
+    return ret
+
+
+leap_binder.add_custom_metric(custom_confusion_metric, "custom_confusion_metric")
--- tests/fixtures/results/simple_no_cloud_wt_visualizer_dataset.py
+++ tests/fixtures/results/simple_no_cloud_wt_visualizer_dataset.py
@@ -3,37 +3,49 @@ import pytest
 from code_loader.contract.enums import DatasetMetadataType, LeapDataType
 from code_loader.contract.responsedataclasses import DatasetSetup, DatasetInputInstance, DatasetMetadataInstance, \
     DatasetOutputInstance, DatasetIntegParseResult, DatasetTestResultPayload, DatasetPreprocess, VisualizerInstance, \
-    PredictionTypeInstance
+    PredictionTypeInstance, MetricInstance
+
+expected_setup = DatasetSetup(
+    inputs=[
+        DatasetInputInstance(name='normal_input_subset_1_10', shape=[1])],
+    metadata=[
+        DatasetMetadataInstance(name='x', type=DatasetMetadataType.int),
+        DatasetMetadataInstance(name='y', type=DatasetMetadataType.string)],
+    outputs=[
+        DatasetOutputInstance(name='output_times_20', shape=[1])
+    ],
+    preprocess=DatasetPreprocess(training_length=4, validation_length=2, test_length=1),
+    visualizers=[
+        VisualizerInstance(name='Image', type=LeapDataType.Image, arg_names=['data']),
+        VisualizerInstance(name='Graph', type=LeapDataType.Graph, arg_names=['data']),
+        VisualizerInstance(name='RawData', type=LeapDataType.Text, arg_names=['data']),
+        VisualizerInstance(name='HorizontalBar', type=LeapDataType.HorizontalBar, arg_names=['data']),
+        VisualizerInstance(name='Text', type=LeapDataType.Text, arg_names=['data']),
+        VisualizerInstance(name='ImageMask', type=LeapDataType.ImageMask, arg_names=['mask', 'image']),
+        VisualizerInstance(name='TextMask', type=LeapDataType.TextMask, arg_names=['mask', 'text_data']),
+        VisualizerInstance(name='stub_visualizer', type=LeapDataType.Text, arg_names=['data'])
+
+    ],
+    metrics=[
+        MetricInstance(name='MeanSquaredError', arg_names=['ground_truth', 'prediction']),
+        MetricInstance(name='MeanSquaredLogarithmicError', arg_names=['ground_truth', 'prediction']),
+        MetricInstance(name='MeanAbsoluteError', arg_names=['ground_truth', 'prediction']),
+        MetricInstance(name='MeanAbsolutePercentageError', arg_names=['ground_truth', 'prediction']),
+        MetricInstance(name='Accuracy', arg_names=['ground_truth', 'prediction']),
+        MetricInstance(name='BinaryAccuracy', arg_names=['ground_truth', 'prediction']),
+        MetricInstance(name='ConfusionMatrixClassification', arg_names=['ground_truth', 'prediction']),
+        MetricInstance(name='MeanIOU', arg_names=['ground_truth', 'prediction']),
+        MetricInstance(name='custom_metric', arg_names=['pred', 'gt']),
+        MetricInstance(name='custom_confusion_metric', arg_names=['gt_one_hot_encoding', 'pred_probabilities']),
+    ],
+    prediction_types=[
+        PredictionTypeInstance('pred_type1', ['yes', 'no'])],
+    custom_loss_names=[]
+)
 
 
 @pytest.fixture
 def no_cloud_wt_visualizer_dataset_loader_expected_result() -> DatasetIntegParseResult:
-    expected_setup = DatasetSetup(
-        inputs=[
-            DatasetInputInstance(name='normal_input_subset_1_10', shape=[1])],
-        metadata=[
-            DatasetMetadataInstance(name='x', type=DatasetMetadataType.int),
-            DatasetMetadataInstance(name='y', type=DatasetMetadataType.string)],
-        outputs=[
-            DatasetOutputInstance(name='output_times_20', shape=[1])
-        ],
-        preprocess=DatasetPreprocess(training_length=4, validation_length=2, test_length=1),
-        visualizers=[
-            VisualizerInstance(name='Image', type=LeapDataType.Image, arg_names=['data']),
-            VisualizerInstance(name='Graph', type=LeapDataType.Graph, arg_names=['data']),
-            VisualizerInstance(name='RawData', type=LeapDataType.Text, arg_names=['data']),
-            VisualizerInstance(name='HorizontalBar', type=LeapDataType.HorizontalBar, arg_names=['data']),
-            VisualizerInstance(name='Text', type=LeapDataType.Text, arg_names=['data']),
-            VisualizerInstance(name='ImageMask', type=LeapDataType.ImageMask, arg_names=['mask', 'image']),
-            VisualizerInstance(name='TextMask', type=LeapDataType.TextMask, arg_names=['mask', 'text_data']),
-            VisualizerInstance(name='stub_visualizer', type=LeapDataType.Text, arg_names=['data'])
-
-        ],
-        prediction_types=[
-            PredictionTypeInstance('pred_type1', ['yes', 'no'])],
-        custom_loss_names=[]
-    )
-
     expected_payloads = [
         DatasetTestResultPayload(name='preprocess', display={}, is_passed=True, shape=None),
         DatasetTestResultPayload(name='normal_input_subset_1_10',
