4ace34fce900ba1bcc89b3e9dc3275995063be7a
Asaf Yehezkel <asaf.yehezkel@tensorleap.ai>
2023-03-20 12:22:51 +0200

    load dataset from path (#89)
    * Leap loader support path alonside script
    
    * Align tests to works with path
    
    * Bump version

--- tests/fixtures/dataset_loaders.py
+++ tests/fixtures/dataset_loaders.py
@@ -8,35 +8,27 @@ from tests.fixtures.dataset_integ_scripts import dataset_integ_scripts_path
 
 @pytest.fixture
 def no_cloud_dataset_loader() -> LeapLoader:
-    script_path = os.path.join(dataset_integ_scripts_path, "simple_no_cloud_dataset.py")
-    with open(script_path, "r") as f:
-        script = f.read()
-    leap_loader = LeapLoader(script)
+    file_name = "simple_no_cloud_dataset.py"
+    leap_loader = LeapLoader('', dataset_integ_scripts_path, file_name)
     return leap_loader
 
 
 @pytest.fixture
 def no_cloud_wt_visualizer_dataset_loader() -> LeapLoader:
-    script_path = os.path.join(dataset_integ_scripts_path, "simple_no_cloud_dataset_wt_visualizer.py")
-    with open(script_path, "r") as f:
-        script = f.read()
-    leap_loader = LeapLoader(script)
+    file_name = "simple_no_cloud_dataset_wt_visualizer.py"
+    leap_loader = LeapLoader('', dataset_integ_scripts_path, file_name)
     return leap_loader
 
 
 @pytest.fixture
 def secret_dataset_loader() -> LeapLoader:
-    script_path = os.path.join(dataset_integ_scripts_path, "simple_dataset_wt_secret.py")
-    with open(script_path, "r") as f:
-        script = f.read()
-    leap_loader = LeapLoader(script)
+    file_name = "simple_dataset_wt_secret.py"
+    leap_loader = LeapLoader('', dataset_integ_scripts_path, file_name)
     return leap_loader
 
 
 @pytest.fixture
 def word_idx_dataset_loader() -> LeapLoader:
-    script_path = os.path.join(dataset_integ_scripts_path, "simple_no_cloud_dataset_wt_word_idx.py")
-    with open(script_path, "r") as f:
-        script = f.read()
-    leap_loader = LeapLoader(script)
+    file_name = "simple_no_cloud_dataset_wt_word_idx.py"
+    leap_loader = LeapLoader('', dataset_integ_scripts_path, file_name)
     return leap_loader
