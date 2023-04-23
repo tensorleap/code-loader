4084d869fbf481cdc3cb1f2016afc573d083fb99
Asaf Yehezkel <asaf.yehezkel@tensorleap.ai>
2023-04-23 11:48:49 +0300

    Ds code as path only (#95)
    * Remove script field from leap loader
    
    * Align unit tests
    
    * Bump version

--- tests/fixtures/dataset_loaders.py
+++ tests/fixtures/dataset_loaders.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 from code_loader.leaploader import LeapLoader
@@ -9,26 +7,26 @@ from tests.fixtures.dataset_integ_scripts import dataset_integ_scripts_path
 @pytest.fixture
 def no_cloud_dataset_loader() -> LeapLoader:
     file_name = "simple_no_cloud_dataset.py"
-    leap_loader = LeapLoader('', dataset_integ_scripts_path, file_name)
+    leap_loader = LeapLoader(dataset_integ_scripts_path, file_name)
     return leap_loader
 
 
 @pytest.fixture
 def no_cloud_wt_visualizer_dataset_loader() -> LeapLoader:
     file_name = "simple_no_cloud_dataset_wt_visualizer.py"
-    leap_loader = LeapLoader('', dataset_integ_scripts_path, file_name)
+    leap_loader = LeapLoader(dataset_integ_scripts_path, file_name)
     return leap_loader
 
 
 @pytest.fixture
 def secret_dataset_loader() -> LeapLoader:
     file_name = "simple_dataset_wt_secret.py"
-    leap_loader = LeapLoader('', dataset_integ_scripts_path, file_name)
+    leap_loader = LeapLoader(dataset_integ_scripts_path, file_name)
     return leap_loader
 
 
 @pytest.fixture
 def word_idx_dataset_loader() -> LeapLoader:
     file_name = "simple_no_cloud_dataset_wt_word_idx.py"
-    leap_loader = LeapLoader('', dataset_integ_scripts_path, file_name)
+    leap_loader = LeapLoader(dataset_integ_scripts_path, file_name)
     return leap_loader
