d7971d73608dbd063dc62ddbc4e93c3f1c81b190
dorhar <doron.harnoy@tensorleap.ai>
2023-08-20 17:17:08 +0300

    rename leap_bninder folder to remove ambiguity

--- code-loader/code_loader/__init__.py
+++ code-loader/code_loader/__init__.py
@@ -1,2 +1,2 @@
 from code_loader.leaploader import LeapLoader
-from code_loader.leap_binder import global_leap_binder as leap_binder
+from code_loader.inner_leap_binder import global_leap_binder as leap_binder
