32d31ce9b363c4ebd09a0025e168ace5f89cd328
Chen Rothschild <chen.rothschild@tensorleap.ai>
2024-08-20 10:25:25 +0300

    chaneg to type error exception

--- code-loader.helpers/code_loader/helpers/store/metadata/image.py
+++ code-loader.helpers/code_loader/helpers/store/metadata/image.py
@@ -24,7 +24,7 @@ NDArray[np.int32]], function_name: str, expected_channels: int, expected_type: O
             Exception: If the image data type is not one of the allowed types.
         """
     if not isinstance(image, np.ndarray):
-        raise NotImplementedError(
+        raise TypeError(
             f"Wrong input type send to metadata {function_name}: Expected numpy array Got {type(image)}")
 
     if expected_channels == 3:
