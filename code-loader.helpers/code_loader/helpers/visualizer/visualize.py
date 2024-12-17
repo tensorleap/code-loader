d6a442624c44022c6a28710ea2444eeccd7ba44b
Chen Rothschild <chen.rothschild@tensorleap.ai>
2024-12-17 15:37:27 +0200

    add title to main visualize function

--- code-loader.helpers/code_loader/helpers/visualizer/visualize.py
+++ code-loader.helpers/code_loader/helpers/visualizer/visualize.py
@@ -2,12 +2,15 @@ import sys
 
 from code_loader.contract.datasetclasses import LeapData  # type: ignore
 from code_loader.helpers.visualizer.plot_functions import plot_switch
+from typing import Optional
 
 
-def visualize(leap_data: LeapData) -> None:
+def visualize(leap_data: LeapData, title: Optional[str] = None) -> None:
     vis_function = plot_switch.get(leap_data.type)
     if vis_function is None:
         print(f"Error: leap data type is not supported, leap data type: {leap_data.type}")
         sys.exit(1)
 
-    vis_function(leap_data)
+    if not title:
+        title = f"Leap {leap_data.type.name} Visualization"
+    vis_function(leap_data, title)
